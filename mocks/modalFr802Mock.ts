1d85af948bc155e3297af33d25e6f4d0c2076abf
Diego Sánchez <sanchezbrisadiego@gmail.com>
2024-05-02 13:11:19 -0300

    modalfr802 confirmation page

---
+++ mocks/modalFr802Mock.ts
@@ -0,0 +1,56 @@
+export const modalFr802 = {
+  accidentType: {
+    "Daños al buque": true,
+    "Daños a terceros": false,
+    "Daños al medio ambiente": true,
+    "Hecho potencialmente peligroso": true,
+  },
+  accidentDescription: {
+    accidentTime: {
+      year: "2024",
+      month: "monthsSelectId-Diciembre",
+      day: "25",
+      hour: "22",
+    },
+    accidentPlace: "Casco del barco",
+    LE: "",
+  },
+  shipCondition: {
+    "Calado proa": false,
+    "Calado popa": true,
+  },
+  weatherStatus: {
+    windPower: "windPower-1",
+    windDirection: "windDirection-Sur",
+    seaPower: "seaPower-3",
+    seaDirection: "seaDirection-Sur",
+    seaCurrentPower: "seaCurrentPower-2",
+    seaCurrentDirection: "seaCurrentDirection-Sur",
+    tideHeight: "1",
+  },
+  HC: {
+    HC: "hydrocarbonsSelect-Si",
+    HCType: "hydrocarbonsTypesSelect-Grasa",
+    HCAmmount: "10",
+    HCActions: "Se intentó recoger de nuevo lo derramado del barco",
+  },
+  accidentVerifications:
+    "Explosión en la cocina del barco, y salió despedida grasa para todo el mar, en la parte de adelante",
+  accidentCaptainOpinion:
+    "El capitán optó por volver a tierra firme para evaluar la situación, ya que estaban cerca y no quería arriesgarse a que el accidente afecte el viaje.",
+  witness: [
+    {
+      id: 1,
+      name: "NombreTrip1",
+      lastName: "ApellidoTrip1",
+    },
+    {
+      id: 3,
+      name: "NombreTrip3",
+      lastName: "ApellidoTrip3",
+    },
+  ],
+  shipStatus: {
+    shipStatus: "En navegación",
+  },
+};
